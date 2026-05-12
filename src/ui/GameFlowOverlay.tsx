9aa6706a14b64f18221e43ec1818da2b3dd059be
petersimmalugnt <peter@simmalugnt.se>
2026-05-12 14:00:13 +0200

    feat: add blink animation and restructure sub-prompt text in GameFlowOverlay

--- src/ui/GameFlowOverlay.tsx
+++ src/ui/GameFlowOverlay.tsx
@@ -1089,7 +1089,7 @@ export function GameFlowOverlay() {
       <div className="gfo-center-wrap">
         <div className="gfo-idle-prompt-wrap">
           <div className="popdot-text-base popdot-style-1 popdot-shadow-8 gfo-idle-prompt gfo-vt-idle-prompt">POP BALLOON TO START!</div>
-          <div className="popdot-text-base popdot-style-2 popdot-shadow-4 gfo-idle-prompt gfo-idle-prompt-sub gfo-vt-idle-prompt-sub">One player at a time.<br />Stand in the spotlight</div>
+          <div className="popdot-text-base popdot-style-3 popdot-shadow-4 gfo-idle-prompt gfo-idle-prompt-sub gfo-vt-idle-prompt-sub"><span>One player at a time.</span><span>Stand in the spotlight.</span></div>
         </div>
       </div>
     )
